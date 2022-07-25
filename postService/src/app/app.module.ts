3e579053999775cbbf191100025194319ea2b3a8
damaris-adesso <damaris.goebel@adesso.de>
2022-07-25 15:31:32 +0200

    post service

---
+++ postService/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UppercaseConverterComponent } from './components/uppercase-converter/uppercase-converter.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UppercaseConverterService } from './services/uppercase-converter.service';
+import {FormsModule} from '@angular/forms'
+
+@NgModule({
+  declarations: [AppComponent, UppercaseConverterComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [UppercaseConverterService],
+  bootstrap: [UppercaseConverterComponent],
+})
+export class AppModule {}
