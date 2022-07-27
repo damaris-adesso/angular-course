d1e25f136bba64ba2b22b12030a43ba28cafa21d
damaris-adesso <damaris.goebel@adesso.de>
2022-07-27 16:00:21 +0200

    reactive forms

---
+++ reactiveForms/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public personForm: FormGroup;
+  public countries: string[];
+
+  ngOnInit() {
+    this.countries = ['USA', 'Germany', 'Italy'];
+    this.personForm = new FormGroup({
+      firstName: new FormControl('John', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10),
+      ]),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      address: new FormGroup({
+        street: new FormControl(),
+        city: new FormControl(),
+        country: new FormControl(),
+      }),
+      gender: new FormControl(),
+    });
+  }
+
+  public onSubmit() {
+    console.log(this.personForm.value);
+    console.log(this.personForm.valid);
+  }
+}
